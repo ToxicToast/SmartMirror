a64a3b8eeb2e75c7f54e1c26798038dea4e882e0
Kortyka Thomas <tkortyka@alegri.eu>
2018-05-14 13:33:35 +0200

    added calendar

--- src/app/date/date.module.ts
+++ src/app/date/date.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DateContainerIndexComponent } from '../date/Containers/date-container-index/date-container-index.component';
 import { ClockComponent } from '../date/Components/clock/clock.component';
+import { CalendarComponent } from '../date/Components/calendar/calendar.component';
 
 @NgModule({
   imports: [
@@ -9,7 +10,8 @@ import { ClockComponent } from '../date/Components/clock/clock.component';
   ],
   declarations: [
     DateContainerIndexComponent,
-    ClockComponent
+    ClockComponent,
+    CalendarComponent
   ],
   exports: [
     DateContainerIndexComponent

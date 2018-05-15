bb1a3b4085d85ed63c32c33ee6bf1437c0d124ee
Kortyka Thomas <tkortyka@alegri.eu>
2018-05-15 15:15:26 +0200

    removed ical

--- src/app/date/Containers/date-container-index/date-container-index.component.ts
+++ src/app/date/Containers/date-container-index/date-container-index.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as ical from 'ical';
 
 @Component({
   selector: 'mirror-date-container-index',
@@ -9,33 +8,16 @@ import * as ical from 'ical';
 export class DateContainerIndexComponent implements OnInit {
 
   currentTime = new Date();
-  calendarUrl: string;
 
-  constructor() {
-    this.calendarUrl = 'https://calendar.google.com/calendar/ical/2frvpfn0p0beldmscmv4p30egc%40group.calendar.google.com/private-9573e48045552b99b9bc5912e5894dff/basic.ics';
-  }
+  constructor() { }
 
   ngOnInit() {
     this.clockTick();
-    this.getCalendar();
   }
 
   clockTick() {
     setInterval(() => {
       this.currentTime = new Date();
     }, 1000);
-  }
-
-  getCalendar() {
-     // Call Calendar State
-    ical.fromURL(this.calendarUrl, {}, (err, data) => {
-      if (err) {
-        console.error(err);
-        // Call Calendar State - Failure
-      } else {
-        console.error(data);
-        // Call Calendar State - Success
-      }
-    });
   }
 }
